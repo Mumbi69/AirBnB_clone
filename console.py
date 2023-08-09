f089f67cb3a3104dc374822ebac91b9afae0c351
Mumbi69 <winfriendmumbi69@gmail.com>
2023-08-09 13:31:09 +0000

    AirBnB clone - The console

--- console.py
+++ console.py
@@ -1,1 +1,29 @@
+#!/usr/bin/python3
+"""Defines the program that contains the entry point of the CLI"""
+
 import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """Representation of the class Cmd from the module cmd."""
+    prompt = "[Air] "
+
+    def emptyline(self):
+        """Empty line that executes nothing"""
+        pass
+
+    def do_quit(self, line):
+        """Quit command to exit the program"""
+        return True
+        print(line)
+
+    def do_EOF(self,line):
+        """The end of file on input"""
+        return True
+
+    def help_quit(self):
+        """Help message for the quit command"""
+        print("Quit command to exit the program")
+        print()
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
