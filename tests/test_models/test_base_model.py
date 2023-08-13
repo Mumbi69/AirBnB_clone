2c9a6fb5a4d3e173c041e7eb8f6dcee3ebc41039
Mumbi69 <winfriendmumbi69@gmail.com>
2023-08-13 23:53:52 +0000

    AirBnB clone - The console

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -23,11 +23,30 @@ class TestBaseModel(unittest.TestCase):
     def test_updated_at_is_public_datetime(self):
         self.assertEqual(datetime, type(BaseModel().updated_at))
 
+    def test_new_instance_stored_in_objects(self):
+        self.assertIn(BaseModel(), models.storage.all().values())
+
     def test_two_models_unique_ids(self):
         b1 = BaseModel()
         b2 = BaseModel()
         self.assertNotEqual(b1.id, b2.id)
 
+    def test_args_unused(self):
+        bm = BaseModel(None)
+        self.assertNotIn(None, bm.__dict__.values())
 
-if __name__ == '__main__':
+    def test_str_representation(self):
+        dt = datetime.today()
+        dt_repr = repr(dt)
+        bm = BaseModel()
+        bm.id = "2345"
+        bm.created_at = bm.updated_at = dt
+        bmstr = bm.__str__()
+        self.assertIn("[BaseModel] (2345)", bmstr)
+        self.assertIn("'id': '2345'", bmstr)
+        self.assertIn("'created_at': " + dt_repr, bmstr)
+        self.assertIn("'updated_at': " + dt_repr, bmstr)
+
+
+if __name__ == "__main__":
     unittest.main()
