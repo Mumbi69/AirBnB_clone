7e9a4117eb74c01495d4d0f4a68ec36e0a49b653
AckimJnr <acklongwe@gmail.com>
2023-08-13 21:42:57 +0200

    Update current date notation

--- models/base_model.py
+++ models/base_model.py
@@ -27,8 +27,8 @@ class BaseModel:
 
         from models import storage
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
         if len(args) == 0:
             storage.new(self)
