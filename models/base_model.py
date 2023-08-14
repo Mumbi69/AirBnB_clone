144d1053e063480a4365c744b2f5cd0147c68c81
AckimJnr <acklongwe@gmail.com>
2023-08-14 20:51:34 +0200

    Update basemode object recreation

--- models/base_model.py
+++ models/base_model.py
@@ -26,7 +26,8 @@ class BaseModel:
                     setattr(self, key, value)
 
         from models import storage
-        self.id = str(uuid.uuid4())
+        if 'id' not in kwargs:
+            self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
