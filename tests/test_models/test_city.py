609dad4e17724969665d337805e91c6b0251593d
Mumbi69 <winfriendmumbi69@gmail.com>
2023-08-14 00:20:11 +0000

    AirBnB clone - The console

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -22,6 +22,26 @@ class TestCity(unittest.TestCase):
     def test_updated_at_is_public_datetime(self):
         self.assertEqual(datetime, type(City().updated_at))
 
+    def test_new_instance_stored_in_objects(self):
+        self.assertIn(City(), models.storage.all().values())
+
+    def test_state_id_is_public_class_attribute(self):
+        cy = City()
+        self.assertEqual(str, type(City.state_id))
+        self.assertIn("state_id", dir(cy))
+        self.assertNotIn("state_id", cy.__dict__)
+
+    def test_name_is_public_class_attribute(self):
+        cy = City()
+        self.assertEqual(str, type(City.name))
+        self.assertIn("name", dir(cy))
+        self.assertNotIn("name", cy.__dict__)
+
+    def test_two_cities_unique_ids(self):
+        cy1 = City()
+        cy2 = City()
+        self.assertNotEqual(cy1.id, cy2.id)
+
 
 if __name__ == "__main__":
     unittest.main()
